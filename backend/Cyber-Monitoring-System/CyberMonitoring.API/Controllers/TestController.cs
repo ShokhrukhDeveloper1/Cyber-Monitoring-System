996c4e4503fcdc9aa46c64770cf1473053060f6e
Shokhrukh <shokhrukhdeveloper1@gmail.com>
2026-01-20 10:14:11 +0300

    INFRA: Initialize API Project

---
+++ backend/Cyber-Monitoring-System/CyberMonitoring.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CyberMonitoring.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Backend is running (Classic .NET 5)!");
+        }
+    }
+}
