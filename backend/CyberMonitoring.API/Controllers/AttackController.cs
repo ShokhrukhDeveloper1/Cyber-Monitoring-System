c9b23f26cd4d39ab6974d3ec664c2dd2e433c767
Shokhrukh <shokhrukhdeveloper1@gmail.com>
2026-01-21 19:13:39 +0300

    backend: add AttackController and AlertController with GET and POST

---
+++ backend/CyberMonitoring.API/Controllers/AttackController.cs
@@ -0,0 +1,37 @@
+﻿using CyberMonitoring.API.Data;
+using CyberMonitoring.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CyberMonitoring.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttackController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AttackController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/attacks
+        [HttpGet]
+        public ActionResult<IEnumerable<AttackLog>> GetAttacks()
+        {
+            return Ok(_context.AttackLogs.ToList());
+        }
+
+        // POST: api/attacks
+        [HttpPost]
+        public ActionResult<AttackLog> CreateAttack([FromBody] AttackLog attack)
+        {
+            _context.AttackLogs.Add(attack);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetAttacks), new { id = attack.Id }, attack);
+        }
+    }
+}
